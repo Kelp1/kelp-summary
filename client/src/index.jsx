424280670c6e57708f2c5feba8b8db20eb18c62c
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-24 19:01:24 -0700

    Minor code-cleanup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,5 +4,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import MapBox from './components/MapBox';
 
-ReactDOM.render(<MapBox className="mapAndPhotos" />, document.getElementById('Map'));
 ReactDOM.render(<App />, document.getElementById('Summary'));
+ReactDOM.render(<MapBox className="mapAndPhotos" />, document.getElementById('Map'));
