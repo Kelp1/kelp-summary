1bcb1adea74b3b3116e7feb0d57c8d1f7d2d2ec0
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-24 17:57:18 -0700

    Fixes tests and adds jest to ESLint

--- specs/sum.test.js
+++ specs/sum.test.js
@@ -1,6 +1,5 @@
-// this is an example
-const sum = require('../client/src/components/sum');
+// example test
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect((1 + 2)).toBe(3);
 });
