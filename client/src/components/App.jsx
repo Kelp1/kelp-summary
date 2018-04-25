2e2cd2dcb537ffc46b98988a153a47c25b10cbd3
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-24 19:53:37 -0700

    Fixed URL for App GET request

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   componentDidMount() {
     const parsed = queryString.parse(window.location.search).id;
-    axios.get(`/api/summary/${parsed}`).then((response) => {
+    axios.get(`http://127.0.0.1:3001/api/summary/${parsed}`).then((response) => {
       this.setState({
         data: response.data,
       });
