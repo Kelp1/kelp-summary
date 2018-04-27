f01959175e9fc52eb5a30d4f59bc6d786e1873f3
Johnny Zhu <zhujohnny@users.noreply.github.com>
2018-04-27 15:33:42 -0700

    Moved star images to S3

--- client/src/components/SummaryInfo.jsx
+++ client/src/components/SummaryInfo.jsx
@@ -18,7 +18,7 @@ function SummaryInfo(props) {
         <span data-toggle="tooltip" title={claimInfo} className="titleLine" id="restaurantClaimed"><svg id="checkmark_badged" height="100%" viewBox="0 0 18 18" width="100%"><path d="M9 1a8 8 0 1 0 0 16A8 8 0 0 0 9 1zm3.96 6.28l-4.808 4.807-3.112-3.11a.8.8 0 1 1 1.13-1.132l1.982 1.98 3.677-3.677a.8.8 0 1 1 1.13 1.13z" /></svg>Claimed</span>
       </div>
       <div className="ratingClass">
-        <img className="reviewLine" id="restaurantStars" src={`./${props.info.stars}.png`} alt={props.info.stars} />
+        <img className="reviewLine" id="restaurantStars" src={`https://s3.amazonaws.com/hrsf93welpusers/${props.info.stars.toString().replace('.','_')}.png`} alt={props.info.stars} />
         <span className="reviewLine" id="restaurantReviewCount">{ props.info.reviewCount } reviews</span>
         <button className="reviewLine" id="restaurantDetail"><svg id="histogram" height="100%" viewBox="0 0 14 14" width="100%"><path d="M9 11V5h2v6H9zM6 3h2v8H6V3zM3 7h2v4H3V7z" /></svg>Detail</button>
       </div>
